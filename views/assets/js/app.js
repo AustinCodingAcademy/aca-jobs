a1b5189b2fcdc1dcfb57e8ba4ae948bcf3724996
Kevin Colten <kevincolten@gmail.com>
2016-10-19 15:47:31 -0500

    Attaching markdown to the window

--- views/assets/js/app.js
+++ views/assets/js/app.js
@@ -1,10 +1,9 @@
 window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
-require('markdown');
+window.markdown = require( "markdown" ).markdown
 require('bootstrap-markdown');
 require('bootstrap-filestyle');
 require('to-markdown');
 require('holderjs');
-require('moment');
 var moment = require('moment');
 window.moment = new moment();
